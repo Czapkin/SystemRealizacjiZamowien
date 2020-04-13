fcd654ab9e36ed7c688c6b993c56ef3ee7fc023e
Maciej <mckmusial@gmail.com>
2020-04-13 23:51:30 +0200

    Test

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WpfApp1
+{
+    public class MySqlConnector
+    {
+        readonly MySqlConnection databaseCon;
+        readonly string inputToConnect;
+        static bool connectionIndicator;
+        public MySqlConnector(string _inputToConnect)
+        {
+            this.inputToConnect = _inputToConnect;
+            databaseCon = null;
+            try
+            {
+                databaseCon = new MySqlConnection(inputToConnect);
+                databaseCon.Open();
+                connectionIndicator = true;
+            }
+            catch (MySqlException err)
+            {
+                connectionIndicator = false;
+                Console.Write(err);
+            }
+            finally
+            {
+                if (databaseCon != null)
+                {
+                    //databaseCon.Close();
+                    //connectionIndicator = false;
+                }
+            }
+        }
+        public DataTable sendRequest(string request)
+        {
+            if(connectionIndicator == true)
+            {
+                DataTable data = new DataTable();
+                
+                MySqlCommand userCommand = new MySqlCommand(request, databaseCon);
+                MySqlDataReader reader = userCommand.ExecuteReader();
+                data.Load(reader);
+                Console.WriteLine("Dasdsadsa");
+                return data;
+            }
+            return new DataTable();   //Nie powiodlo sie
+        }
+        public bool isConnected()
+        {
+            if (connectionIndicator == true) return true;
+            else return false;
+        }
+
+    }
+    
+    public class Class1 : Button
+    {
+        public string name;
+        public string price;
+
+        public Class1(string name, string price)
+        {
+            this.name = name;
+            this.price = price;
+            setContent(this.name);
+        }
+
+        public void setContent(string name)
+        {
+            Content = name;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.Data;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MySqlConnector mysql = new MySqlConnector
+                  ("SERVER=localhost;DATABASE=db_system_realizacji_zamowien_posilkow_12042020;UID=root;PASSWORD=WASZEHASLO;");
+            DataTable n1 = new DataTable();
+            n1 = mysql.sendRequest("SELECT * from tb_sets");
+
+            foreach (DataRow dbRow in n1.Rows)
+            {
+                foreach (DataColumn dbColumns in n1.Columns)
+                {
+                    var field1 = dbRow[dbColumns].ToString();
+                    Console.WriteLine(field1);
+                }
+            }
+            
+            var name = new List<string>()
+            {
+                "Ham",
+                "Cheese",
+                "NugBg",
+                "CrsChk",
+                "ChiCheese",
+                "dasdsadsa",
+            };
+
+            var price = new List<string>()
+            {
+                "5",
+                "10",
+                "15",
+                "20",
+                "25",
+                "33",
+            };
+
+            for(int i=0; i<name.Count; i++)
+            {
+                Class1 button = new Class1(name[i], price[i])
+                {
+                    Tag = price[i]
+                };
+
+                button.Click += new RoutedEventHandler(button_Click);
+                grid.Children.Add(button);
+            }
+        }
+
+        void button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(string.Format("The price of the selected product is:  {0}.", (sender as Class1).Tag));
+        }
+    }
+}
