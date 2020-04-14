eea6e13bc76c1cdd1b87fefe103a516f6d63fb29
Maciej <mckmusial@gmail.com>
2020-04-14 18:03:06 +0200

    Poprawki do errora

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -26,85 +26,90 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
-            start:    // na razie niech bedzie goto
-            MySqlConnector mysql = new MySqlConnector
-                  ("SERVER=localhost;DATABASE=db_system_realizacji_zamowien_posilkow_12042020;UID=root;PASSWORD=CHUJKurwa");
-            DataTable n1 = new DataTable();
-            DataTable n2 = new DataTable();
-            DataTable n3 = new DataTable(); // kontenery dla wynikow zapytan
-            DataTable n4 = new DataTable();
-            DataTable n5 = new DataTable();
+            string loginString =
+                "SERVER=localhost;DATABASE=db_system_realizacji_zamowien_posilkow_12042020;UID=root;PASSWORD=Chuj";
 
+                // TU WYZEJ WPISUJCIE HASLO
 
-            if (mysql.isConnected())
+            MySqlConnector mysql = new MySqlConnector(loginString);
+
+            while (!mysql.isConnected())   // sprawdza czy istnieje polaczenie z baza 
             {
+                MessageBoxResult result = MessageBox.Show("Failed connecting to database\nTry again?", "ERROR"
+                    , MessageBoxButton.YesNo);
+                mysql = new MySqlConnector(loginString);
+                switch (result)
+                {
+                    case MessageBoxResult.No:
+                        {
+                            System.Environment.Exit(0);
+                            break;
+                        }
+                    case MessageBoxResult.Yes:
+                        {
+                            continue;
+                        }
+
+                }
+            }
+
+                DataTable n1 = new DataTable();
+                DataTable n2 = new DataTable();
+                DataTable n3 = new DataTable(); // kontenery dla wynikow zapytan
+                DataTable n4 = new DataTable();
+                DataTable n5 = new DataTable();
+
                 n1 = mysql.sendRequest("SELECT * from tb_beverages");
                 n2 = mysql.sendRequest("SELECT * from tb_desserts");
                 n3 = mysql.sendRequest("SELECT * from tb_sandwiches");
                 n4 = mysql.sendRequest("SELECT * from tb_sets");
                 n5 = mysql.sendRequest("SELECT * from tb_snacks");
-            }
-            else
-            {
-                MessageBoxResult result = MessageBox.Show("Failed connecting to database\nTry again?", "ERROR",MessageBoxButton.YesNo);
-                switch (result)
+
+                var WholeProduct = new List<string>(); // znajduja sie tu wszystkie elementy z bazy danych(cena, nazwa itd)
+                loopThroughDataT(n1, WholeProduct);
+                loopThroughDataT(n2, WholeProduct);    // zapelniamy liste stringow WholeProduct wszystkimi elementami z bazy
+                loopThroughDataT(n3, WholeProduct);    // implementacja funkcji wyzej.
+                loopThroughDataT(n4, WholeProduct);
+                loopThroughDataT(n5, WholeProduct);
+
+                var productName = new List<string>();
+
+                for (int i = 1; i <= WholeProduct.Count; i += 7) // nazwa produktu w bazie danych jest pierwsza (liczac od 0)
+                {                                                // iterujemy po tablicy ktora zawiera wszystkie elementy bazy 
+                    productName.Add((WholeProduct[i]));          // (lacznie 7) dzieki czemu w tej petli dodajemy tylko nazwy produkt.
+                }
+
+                var productCost = new List<string>();       // robimy to samo tylko dla cen
+
+                for (int i = 5; i <= WholeProduct.Count; i += 7)
                 {
-                    case MessageBoxResult.Yes:
-                        goto start;    // niech bedzie na razie na goto
-                        break;
-                    case MessageBoxResult.No:
-                        this.Close();
-                        break;
+                    productCost.Add(WholeProduct[i]);
+                }
+
+
+                var name = productName;   //przypisujemy elementy listy do zmiennych name i price
+                var price = productCost;
+
+                for (int i = 0; i < name.Count; i++)
+                {
+                    Class1 button = new Class1(name[i], price[i])       // dalsza czesc to automatyczna generacja okienek
+                    {
+                        Tag = price[i]
+                    };
+
+                    button.Click += new RoutedEventHandler(button_Click);
+                    grid.Children.Add(button);
                 }
             }
-        
 
 
-            var WholeProduct = new List<string>(); // znajduja sie tu wszystkie elementy z bazy danych(cena, nazwa itd)
-            loopThroughDataT(n1, WholeProduct);
-            loopThroughDataT(n2, WholeProduct);    // zapelniamy liste stringow WholeProduct wszystkimi elementami z bazy
-            loopThroughDataT(n3, WholeProduct);    // implementacja funkcji wyzej.
-            loopThroughDataT(n4, WholeProduct);
-            loopThroughDataT(n5, WholeProduct);
 
-            var productName = new List<string>();
 
-            for (int i = 1; i <= WholeProduct.Count; i += 7) // nazwa produktu w bazie danych jest pierwsza (liczac od 0)
-            {                                                // iterujemy po tablicy ktora zawiera wszystkie elementy bazy 
-                productName.Add((WholeProduct[i]));          // (lacznie 7) dzieki czemu w tej petli dodajemy tylko nazwy produkt.
-            }
-
-            var productCost = new List<string>();       // robimy to samo tylko dla cen
-
-            for (int i = 5; i <= WholeProduct.Count; i += 7)
+            void button_Click(object sender, RoutedEventArgs e)         // co sie stanie gdy nacisniemy na element
             {
-                productCost.Add(WholeProduct[i]);
-            }
-
-
-            var name = productName;   //przypisujemy elementy listy do zmiennych name i price
-            var price = productCost;
-
-            for (int i = 0; i < name.Count; i++)
-            {
-                Class1 button = new Class1(name[i], price[i])       // dalsza czesc to automatyczna generacja okienek
-                {
-                    Tag = price[i]
-                };
-
-                button.Click += new RoutedEventHandler(button_Click);
-                grid.Children.Add(button);
+                Console.WriteLine(string.Format("The price of the selected product is:  {0}.", (sender as Class1).Tag));
             }
         }
-
-    
-
-
-        void button_Click(object sender, RoutedEventArgs e)         // co sie stanie gdy nacisniemy na element
-        {
-            Console.WriteLine(string.Format("The price of the selected product is:  {0}.", (sender as Class1).Tag));
-        }
-    }
 }
 
 
