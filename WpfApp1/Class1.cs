c5d85657d6c9d56b422bf5990bd26762682e2915
Maciej <mckmusial@gmail.com>
2020-04-14 17:05:35 +0200

    Okienko failed to connect to DB

--- WpfApp1/Class1.cs
+++ WpfApp1/Class1.cs
@@ -8,57 +8,7 @@ using System.Windows.Controls;
 
 namespace WpfApp1
 {
-    /*
-    public class MySqlConnector
-    {
-        readonly MySqlConnection databaseCon;
-        readonly string inputToConnect;
-        static bool connectionIndicator;
-        public MySqlConnector(string _inputToConnect)
-        {
-            this.inputToConnect = _inputToConnect;
-            databaseCon = null;
-            try
-            {
-                databaseCon = new MySqlConnection(inputToConnect);
-                databaseCon.Open();
-                connectionIndicator = true;
-            }
-            catch (MySqlException err)
-            {
-                connectionIndicator = false;
-                Console.Write(err);
-            }
-            finally
-            {
-                if (databaseCon != null)
-                {
-                    //databaseCon.Close();
-                    //connectionIndicator = false;
-                }
-            }
-        }
-        public DataTable sendRequest(string request)
-        {
-            if(connectionIndicator == true)
-            {
-                DataTable data = new DataTable();
-                
-                MySqlCommand userCommand = new MySqlCommand(request, databaseCon);
-                MySqlDataReader reader = userCommand.ExecuteReader();
-                data.Load(reader);
-                return data;
-            }
-            return new DataTable();   //Nie powiodlo sie
-        }
-        public bool isConnected()
-        {
-            if (connectionIndicator == true) return true;
-            else return false;
-        }
-
-    }
-    */
+    
     public class Class1 : Button
     {
         public string name;
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,61 +27,72 @@ namespace WpfApp1
         {
             InitializeComponent();
 
-            MySqlConnector mysql = new MySqlConnector 
-                  ("SERVER=localhost;DATABASE=db_system_realizacji_zamowien_posilkow_12042020;UID=root;PASSWORD=WASZE");
+            MySqlConnector mysql = new MySqlConnector
+                  ("SERVER=localhost;DATABASE=db_system_realizacji_zamowien_posilkow_12042020;UID=root;PASSWORD=WAASZE");
             DataTable n1 = new DataTable();
             DataTable n2 = new DataTable();
             DataTable n3 = new DataTable(); // kontenery dla wynikow zapytan
             DataTable n4 = new DataTable();
             DataTable n5 = new DataTable();
 
-            n1 = mysql.sendRequest("SELECT * from tb_beverages"); 
-            n2 = mysql.sendRequest("SELECT * from tb_desserts");
-            n3 = mysql.sendRequest("SELECT * from tb_sandwiches");
-            n4 = mysql.sendRequest("SELECT * from tb_sets");
-            n5 = mysql.sendRequest("SELECT * from tb_snacks");
-
-
-            var WholeProduct = new List<string>(); // znajduja sie tu wszystkie elementy z bazy danych(cena, nazwa itd)
-            loopThroughDataT(n1, WholeProduct);
-            loopThroughDataT(n2, WholeProduct);    // zapelniamy liste stringow WholeProduct wszystkimi elementami z bazy
-            loopThroughDataT(n3, WholeProduct);    // implementacja funkcji wyzej.
-            loopThroughDataT(n4, WholeProduct);
-            loopThroughDataT(n5, WholeProduct);
-
-            var productName = new List<string>();
-
-            for (int i = 1; i <= WholeProduct.Count; i += 7) // nazwa produktu w bazie danych jest pierwsza (liczac od 0)
-            {                                                // iterujemy po tablicy ktora zawiera wszystkie elementy bazy 
-                productName.Add((WholeProduct[i]));          // (lacznie 7) dzieki czemu w tej petli dodajemy tylko nazwy produkt.
-            }
-
-            var productCost = new List<string>();       // robimy to samo tylko dla cen
-
-            for(int i = 5; i <= WholeProduct.Count; i += 7)
+            if (mysql.isConnected())
             {
-                productCost.Add(WholeProduct[i]);
-            }
+                n1 = mysql.sendRequest("SELECT * from tb_beverages");
+                n2 = mysql.sendRequest("SELECT * from tb_desserts");
+                n3 = mysql.sendRequest("SELECT * from tb_sandwiches");
+                n4 = mysql.sendRequest("SELECT * from tb_sets");
+                n5 = mysql.sendRequest("SELECT * from tb_snacks");
 
 
-            var name = productName;   //przypisujemy elementy listy do zmiennych name i price
-            var price = productCost;
-            
-            for(int i=0; i<name.Count; i++)
-            {
-                Class1 button = new Class1(name[i], price[i])       // dalsza czesc to automatyczna generacja okienek
+
+                var WholeProduct = new List<string>(); // znajduja sie tu wszystkie elementy z bazy danych(cena, nazwa itd)
+                loopThroughDataT(n1, WholeProduct);
+                loopThroughDataT(n2, WholeProduct);    // zapelniamy liste stringow WholeProduct wszystkimi elementami z bazy
+                loopThroughDataT(n3, WholeProduct);    // implementacja funkcji wyzej.
+                loopThroughDataT(n4, WholeProduct);
+                loopThroughDataT(n5, WholeProduct);
+
+                var productName = new List<string>();
+
+                for (int i = 1; i <= WholeProduct.Count; i += 7) // nazwa produktu w bazie danych jest pierwsza (liczac od 0)
+                {                                                // iterujemy po tablicy ktora zawiera wszystkie elementy bazy 
+                    productName.Add((WholeProduct[i]));          // (lacznie 7) dzieki czemu w tej petli dodajemy tylko nazwy produkt.
+                }
+
+                var productCost = new List<string>();       // robimy to samo tylko dla cen
+
+                for (int i = 5; i <= WholeProduct.Count; i += 7)
                 {
-                    Tag = price[i]
-                };
+                    productCost.Add(WholeProduct[i]);
+                }
 
-                button.Click += new RoutedEventHandler(button_Click);
-                grid.Children.Add(button);
+
+                var name = productName;   //przypisujemy elementy listy do zmiennych name i price
+                var price = productCost;
+
+                for (int i = 0; i < name.Count; i++)
+                {
+                    Class1 button = new Class1(name[i], price[i])       // dalsza czesc to automatyczna generacja okienek
+                    {
+                        Tag = price[i]
+                    };
+
+                    button.Click += new RoutedEventHandler(button_Click);
+                    grid.Children.Add(button);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Brak połączenia z bazą danych");
+                this.Close();
             }
         }
 
-        void button_Click(object sender, RoutedEventArgs e)         // co sie stanie gdy nacisniemy na element
-        {
-            Console.WriteLine(string.Format("The price of the selected product is:  {0}.", (sender as Class1).Tag));
-        }
+
+            void button_Click(object sender, RoutedEventArgs e)         // co sie stanie gdy nacisniemy na element
+            {
+                Console.WriteLine(string.Format("The price of the selected product is:  {0}.", (sender as Class1).Tag));
+            }     
+        
     }
 }
