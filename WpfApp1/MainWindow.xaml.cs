10a17603714053f8813fdf9c8a0503c039bbf388
Maciej <mckmusial@gmail.com>
2020-04-15 19:22:07 +0200

    Zmiany kosmetyczne

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -48,11 +48,22 @@ namespace WpfApp1
             }
         }
 
+        public List<string> parseDataFromDataTable(int indexOfProdInDB, List<string>WholeProducts)
+        {
+            List<string> temporaryArr = new List<string>();
+            
+            for (; indexOfProdInDB <= WholeProducts.Count; indexOfProdInDB += 7) 
+            {                                                
+                temporaryArr.Add((WholeProducts[indexOfProdInDB]));          
+            }
+            return temporaryArr;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
             string loginString =
-                "SERVER=localhost;DATABASE=db_system_realizacji_zamowien_posilkow_12042020;UID=root;PASSWORD=dsadas";
+                "SERVER=localhost;DATABASE=db_system_realizacji_zamowien_posilkow_12042020;UID=root;PASSWORD=das";
 
                 // TU WYZEJ WPISUJCIE HASLO
 
@@ -64,44 +75,39 @@ namespace WpfApp1
             }
 
 
-                DataTable n1 = new DataTable();
-                DataTable n2 = new DataTable();
-                DataTable n3 = new DataTable(); // kontenery dla wynikow zapytan
-                DataTable n4 = new DataTable();
-                DataTable n5 = new DataTable();
+            DataTable n1 = new DataTable();
+            DataTable n2 = new DataTable();
+            DataTable n3 = new DataTable(); // kontenery dla wynikow zapytan
+            DataTable n4 = new DataTable();
+            DataTable n5 = new DataTable();
 
-                n1 = mysql.sendRequest("SELECT * from tb_beverages");
-                n2 = mysql.sendRequest("SELECT * from tb_desserts");
-                n3 = mysql.sendRequest("SELECT * from tb_sandwiches");
-                n4 = mysql.sendRequest("SELECT * from tb_sets");
-                n5 = mysql.sendRequest("SELECT * from tb_snacks");
+            n1 = mysql.sendRequest("SELECT * from tb_beverages");
+            n2 = mysql.sendRequest("SELECT * from tb_desserts");
+            n3 = mysql.sendRequest("SELECT * from tb_sandwiches");
+            n4 = mysql.sendRequest("SELECT * from tb_sets");
+            n5 = mysql.sendRequest("SELECT * from tb_snacks");
 
-                var WholeProduct = new List<string>(); // znajduja sie tu wszystkie elementy z bazy danych(cena, nazwa itd)
-                loopThroughDataT(n1, WholeProduct);
-                loopThroughDataT(n2, WholeProduct);    // zapelniamy liste stringow WholeProduct wszystkimi elementami z bazy
-                loopThroughDataT(n3, WholeProduct);    // implementacja funkcji wyzej.
-                loopThroughDataT(n4, WholeProduct);
-                loopThroughDataT(n5, WholeProduct);
+            var WholeProduct = new List<string>(); // znajduja sie tu wszystkie elementy z bazy danych(cena, nazwa itd)
+            loopThroughDataT(n1, WholeProduct);
+            loopThroughDataT(n2, WholeProduct);    // zapelniamy liste stringow WholeProduct wszystkimi elementami z bazy
+            loopThroughDataT(n3, WholeProduct);    // implementacja funkcji wyzej.
+            loopThroughDataT(n4, WholeProduct);
+            loopThroughDataT(n5, WholeProduct);
 
-                var productName = new List<string>();
+            int iterator = 0;                
+            List<string>productName = parseDataFromDataTable(iterator = 1, WholeProduct);
+            List<string>productCost = parseDataFromDataTable(iterator = 5, WholeProduct);
+                
+            // nazwa produktu w bazie danych jest druga (liczac od 0)
+            // iterujemy po tablicy ktora zawiera wszystkie elementy bazy 
+            // (lacznie 7) dzieki czemu w tej petli dodajemy tylko nazwy produkt.
+            // np pierwsze (nie liczac 0) pole w bazie danych to nazwa produktu 
+            // piąte to jego koszt, cyklicznie zmieniają się zawsze o 7 (wszystkie pola)
 
-                for (int i = 1; i <= WholeProduct.Count; i += 7) // nazwa produktu w bazie danych jest pierwsza (liczac od 0)
-                {                                                // iterujemy po tablicy ktora zawiera wszystkie elementy bazy 
-                    productName.Add((WholeProduct[i]));          // (lacznie 7) dzieki czemu w tej petli dodajemy tylko nazwy produkt.
-                }
+            var name = productName;   //przypisujemy elementy listy do zmiennych name i price
+            var price = productCost;
 
-                var productCost = new List<string>();       // robimy to samo tylko dla cen
-
-                for (int i = 5; i <= WholeProduct.Count; i += 7)
-                {
-                    productCost.Add(WholeProduct[i]);
-                }
-
-
-                var name = productName;   //przypisujemy elementy listy do zmiennych name i price
-                var price = productCost;
-
-                for (int i = 0; i < name.Count; i++)
+            for (int i = 0; i < name.Count; i++)
                 {
                     Class1 button = new Class1(name[i], price[i])       // dalsza czesc to automatyczna generacja okienek
                     {
@@ -110,14 +116,14 @@ namespace WpfApp1
 
                     button.Click += new RoutedEventHandler(button_Click);
                     grid.Children.Add(button);
-                }
-            }
+                }  
+        }
 
 
 
 
-            void button_Click(object sender, RoutedEventArgs e)         // co sie stanie gdy nacisniemy na element
-            {
+        void button_Click(object sender, RoutedEventArgs e)         // co sie stanie gdy nacisniemy na element
+        {
                 Console.WriteLine(string.Format("The price of the selected product is:  {0}.", (sender as Class1).Tag));
             }
         }
